8a7413a3c42bc166cc8ea8c3af053ac1525430ad
Luciano Emmanuel Gatti Flekenstein <138601174+Luciano-Gatti@users.noreply.github.com>
2026-01-30 10:13:43 -0300

    Restore weather web clients

--- Prediction-App/src/main/java/com/flightontime/app_predictor/infrastructure/config/WebClientConfig.java
+++ Prediction-App/src/main/java/com/flightontime/app_predictor/infrastructure/config/WebClientConfig.java
@@ -1,9 +1,13 @@
 package com.flightontime.app_predictor.infrastructure.config;
 
+import io.netty.channel.ChannelOption;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfig {
@@ -18,6 +22,34 @@ public class WebClientConfig {
     public WebClient airportWebClient(@Value("${airport.service.url}") String airportServiceUrl) {
         return WebClient.builder()
                 .baseUrl(airportServiceUrl)
+                .build();
+    }
+
+    @Bean
+    public WebClient weatherWebClient(
+            @Value("${weather.service.base-url}") String weatherServiceBaseUrl,
+            @Value("${weather.service.timeout.connect}") Duration connectTimeout,
+            @Value("${weather.service.timeout.read}") Duration readTimeout
+    ) {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, Math.toIntExact(connectTimeout.toMillis()))
+                .responseTimeout(readTimeout);
+        return WebClient.builder()
+                .baseUrl(weatherServiceBaseUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+
+    @Bean
+    public WebClient fallbackWeatherWebClient(
+            @Value("${weather.fallback.timeout.connect}") Duration connectTimeout,
+            @Value("${weather.fallback.timeout.read}") Duration readTimeout
+    ) {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, Math.toIntExact(connectTimeout.toMillis()))
+                .responseTimeout(readTimeout);
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build();
     }
 }
