9c6d494a5ae67f6d21bea12aa3f0ae07ea2c87fc
Luciano Emmanuel Gatti Flekenstein <138601174+Luciano-Gatti@users.noreply.github.com>
2026-02-02 09:18:13 -0300

    Make JWT secret base64 configurable

---
+++ Prediction-App/src/main/java/com/flightontime/app_predictor/infrastructure/config/properties/ModelServiceProperties.java
@@ -0,0 +1,16 @@
+package com.flightontime.app_predictor.infrastructure.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "model.service")
+public class ModelServiceProperties {
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
